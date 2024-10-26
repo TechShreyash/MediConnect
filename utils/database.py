d575d0b65885fb0cb6a0da4dcc63c389984a9e98
TechShreyash <techshreyash123@gmail.com>
2024-10-26 11:52:19 +0530

    Added Auth In Database

--- utils/database.py
+++ utils/database.py
@@ -1,4 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from typing import Literal
 
 from config import MONGODB_URL
 from utils.logger import Logger
@@ -10,4 +11,21 @@ DB = AsyncIOMotorClient(MONGODB_URL)["MediConnect"]
 logger.info("Connected to MongoDB")
 
 AUTHDB = DB["AUTHDB"]
-SHOPDB = DB["SHOPDB"]
+
+# For Login Signup
+
+async def new_auth(email: str, password: str,_type:Literal["shop","user"]):
+    if await AUTHDB.find_one({"email": email}):
+        return {'status':False, 'message':"Email already exists"}
+    
+    await AUTHDB.insert_one({"email": email, "password": password,"type":_type})
+    return {'status':True, 'message':"Signup Successful"}
+
+async def check_auth(email: str, password: str):
+    user = await AUTHDB.find_one({"email": email})
+    if user:
+        if user["password"] == password:
+            return {'status':True, 'message':"Login Successful"}
+        return {'status':False, 'message':"Incorrect Password"}
+    
+    return {'status':False, 'message':"Email not found"}
